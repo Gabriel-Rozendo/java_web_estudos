9fd02387fe06937b69c469fa129dccc2ec8782cc
Gabriel Rozendo <gabrielrozendodacosta@gmail.com>
2025-02-14 11:18:29 -0300

    Estudo sobre IF ELSE

---
+++ src/java/javaBasico/ifElse.java
@@ -0,0 +1,14 @@
+package javaBasico;
+
+public class ifElse {
+    public static void main(String[] args) {
+
+        double mediaAluno = 70.0;
+
+        if (mediaAluno >= 70) {
+            System.out.println("Parabéns, você está aprovado!");
+        } else {
+            System.out.println("Você está reprovado");
+        }
+    }
+}
